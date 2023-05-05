0cc3a7d31bdfe3d898b021314071deef54441e63
Vsevolod (Seva) Dolgopolov <zavalit@gmail.com>
2023-05-05 16:39:54 +0200

    add readme

---
+++ ray-marching/Readme.md
@@ -0,0 +1,13 @@
+# ray marching
+
+very simple but powerful 2d ray marching implementation
+
+
+to run it you need:
+````
+// install
+npm install
+// and run
+npx parcel index.html
+
+```
--- ray-marching/shaders/fragment.glsl
+++ ray-marching/shaders/fragment.glsl
@@ -36,7 +36,9 @@ Ray makeRay (vec2 uv, vec2 source) {
   return r;
 }
 
-
+/**
+* from https://iquilezles.org/articles/palettes/
+*/
 vec3 pal( in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d )
 {
     return a + b*cos( 6.28318*(c*t+d) );
@@ -64,11 +66,9 @@ void main() {
   ray.uv -= ray.source;
 
   
-  // light
+  // color
   float c = sdfCircle(ray.uv);
   color = pal( c, vec3(0.5,0.5,0.5),vec3(0.5,0.5,0.5),vec3(2.0,1.0,0.0),vec3(0.5,0.20,0.25) );
-  
-  //color = pal( c, vec3(0.5,0.5,0.5),vec3(0.5,0.5,0.5),vec3(1.0,1.0,1.0),vec3(0.0,0.10,0.20) );
 
   fragColor = vec4(color, 1.0);
 }
